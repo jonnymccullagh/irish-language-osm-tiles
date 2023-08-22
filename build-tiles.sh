61bc4c7b29f077698a51139508722abeb3293fbf
Jonny McCullagh <jonny.mccullagh@redbranch.net>
2023-08-22 15:53:02 +0100

    add action

--- build-tiles.sh
+++ build-tiles.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-PATH_TO_NGINX_CONF=/etc/nginx/sites-available/learscail.openstreetmap.ie.conf
-PATH_TO_WEB_FILES=/var/www/learscail.openstreetmap.ie
-cd $PATH_TO_WEB_FILES
+echo "Start Time: $(date)"
 git clone https://github.com/openmaptiles/openmaptiles.git
 cd openmaptiles
 git checkout v3.14
 sed -i 's/MAX_ZOOM=7/MAX_ZOOM=17/' .env
 make remove-docker-images
+echo "**** Starting DB container ****"
 make start-db
+echo "**** Import Data ****"
 make import-data
+echo "**** Download Area ****"
 make download area="ireland-and-northern-ireland"
+echo "**** Import ****"
 make import-osm area=ireland-and-northern-ireland
 make import-wikidata area=ireland-and-northern-ireland
-cp ./mapping.yaml build/mapping.yaml
+echo "**** Copy mapping file ****"
+cp ../mapping.yaml build/mapping.yaml
+echo "**** Import SQL ****"
 make import-sql 
+echo "**** Build Style ****"
 make build-style
 make generate-bbox-file area=ireland-and-northern-ireland
+echo "**** Generate Tiles ****"
 make generate-tiles-pg
-# Copy files to web directory
-cp -r ./data/tiles.mbtiles $PATH_TO_WEB_FILES/tiles.mbtiles
-
-
-
+echo "End Time: $(date)"
